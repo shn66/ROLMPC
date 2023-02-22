d6a33a6cc2af6b80a1e884bfeddd8c230ec12780
Siddharth Hari Nair <42875310+shn66@users.noreply.github.com>
2023-02-22 13:38:22 -0800

    Add files via upload

--- dynamics/DefineSystem_bicycle.m
+++ dynamics/DefineSystem_bicycle.m
@@ -1,6 +1,6 @@
 function [P, C,U, X, Q, R, N, Ny] = DefineSystem_bicycle()
 % Set controller horizon
-N = 4;
+N = 8;
 
 % Set cost function
 Q = 100;
@@ -22,7 +22,7 @@ U = Polyhedron([u1_max u2_max;u1_max -u2_max;u1_min u2_max;u1_min -u2_max]);
 
 % Define the Recovery input constraint set R
 % Define the state constraint set X
-X=Polyhedron('A',[1 0 0;-1 0 0;0 1 0;0 -1 0; 0 0 1; 0 0 -1], 'b', [70;1.0;5;5;pi/2-pi/60;pi/2-pi/60]);
+X=Polyhedron('A',[1 0 0;-1 0 0;0 1 0;0 -1 0; 0 0 1; 0 0 -1], 'b', [60;1.0;5;5;pi/2-pi/60;pi/2-pi/60]);
 % x_max = 15;
 % v_max = 15;
 % X = Polyhedron([x_max v_max; x_max -v_max; -x_max -v_max; -x_max v_max]);
