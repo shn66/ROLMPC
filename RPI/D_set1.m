e39074595c8e92fb9d83208946771974a799b67e
Siddharth Hari Nair <42875310+shn66@users.noreply.github.com>
2024-11-18 10:52:35 -0800

    updated files

--- RPI/D_set1.m
+++ RPI/D_set1.m
@@ -1,58 +1,54 @@
-function [S,c] = D_set1(d,x,L, gamm, UBx, LBx)
+function [S,c] = D_set1(d,z,L, gamm, UBz, LBz)
+% z contains (x,u) data
+
 T=size(d,2);
 nd=size(d,1);
-nx=size(x,1);
+nz=size(z,1);
 S=sdpvar(nd, nd);
 c=sdpvar(nd,1);
 
-[Px,Psigx,~]=svd(x(:,1:T)-1*mean(x(:,1:T)')');
-% [Px,Psigx,~]=svd(x(:,1:T));
-% Px=Px*Psigx(:,1:nx);
-% Px=eye(nx);
+[Pz,Psigz,~]=svd(z(:,1:T)-1*mean(z(:,1:T)')');
+% Pz=Pz*Psigz(:,1:nz);
+% Pz=eye(nz);
 [Pd,Psigd,~]=svd(d-1*mean(d')');
-% [Pd,Psigd,~]=svd(d);
 % Pd=Pd*Psigd(:,1:nd);
 % Pd=eye(nd);
-Q=blkdiag(8*gamm^2, 2*L^2*eye(nx),-eye(nd));
-% Q=blkdiag(8*gamm^2, 2*L^2*Px'*Px,-Pd'*Pd);
+Q=blkdiag(8*gamm^2, 2*L^2*eye(nz),-eye(nd));
+% Q=blkdiag(8*gamm^2, 2*L^2*Pz'*Pz,-Pd'*Pd);
 
 
-B_x=sdpvar(nx, nx, 'full');
+B_z=sdpvar(nz, nz, 'full');
 
-Mx=[-2*LBx'*B_x*UBx UBx'*B_x'*Px+LBx'*B_x*Px zeros(1,nd);
-     Px'*B_x*UBx+Px'*B_x'*LBx -Px'*B_x*Px-Px'*B_x'*Px zeros(nx, nd);
-     zeros(nd, nx+nd+1)];
-% UBx=Px\UBx;
-% LBx=Px\LBx;
-% Mx=[-2*LBx'*B_x*UBx UBx'*B_x'+LBx'*B_x zeros(1,nd);
-%      B_x*UBx+B_x'*LBx -B_x-B_x' zeros(nx, nd);
-%      zeros(nd, nx+nd+1)];
+Mz=[-2*LBz'*B_z*UBz UBz'*B_z'*Pz+LBz'*B_z*Pz zeros(1,nd);
+     Pz'*B_z*UBz+Pz'*B_z'*LBz -Pz'*B_z*Pz-Pz'*B_z'*Pz zeros(nz, nd);
+     zeros(nd, nz+nd+1)];
 
+% UBz=Pz\UBz;
+% LBz=Pz\LBz;
  
-M11=[1;zeros(nx+nd,1)]*[1;zeros(nx+nd,1)]'-Mx;
+M11=[1;zeros(nz+nd,1)]*[1;zeros(nz+nd,1)]'-Mz;
 t_d=sdpvar(T,1);
 
 for i=1:T
-    Md=(blkdiag(1,Px,Pd)-[0;x(:,i);d(:,i)]*[1 zeros(1,nx+nd)])'*Q*(blkdiag(1,Px,Pd)-[0;x(:,i);d(:,i)]*[1 zeros(1,nx+nd)]);
-%     Md=(eye(1+nd+nx)-blkdiag(1,Px,Pd)\[0;x(:,i);d(:,i)]*[1 zeros(1,nx+nd)])'*Q*(eye(1+nd+nx)-blkdiag(1,Px,Pd)\[0;x(:,i);d(:,i)]*[1 zeros(1,nx+nd)]);
+    Md=(blkdiag(1,Pz,Pd)-[0;z(:,i);d(:,i)]*[1 zeros(1,nz+nd)])'*Q*(blkdiag(1,Pz,Pd)-[0;z(:,i);d(:,i)]*[1 zeros(1,nz+nd)]);
+%     Md=(eye(1+nd+nz)-blkdiag(1,Pz,Pd)\[0;x(:,i);d(:,i)]*[1 zeros(1,nz+nd)])'*Q*(eye(1+nd+nz)-blkdiag(1,Pz,Pd)\[0;x(:,i);d(:,i)]*[1 zeros(1,nz+nd)]);
     M11=M11-t_d(i)*Md; 
 end
 
-% LMI=[M11 [-c';zeros(nx,nd);Pd'];
-%      [-c';zeros(nx,nd);Pd']' S];
+% LMI=[M11 [-c';zeros(nz,nd);Pd'];
+%      [-c';zeros(nz,nd);Pd']' S];
 % 
-LMI=[M11 [-c';zeros(nx,nd);eye(nd)];
-     [-c';zeros(nx,nd);eye(nd)]' S];
-%  
-% constr= [ S>=0; LMI>=0; Px'*B_x*Px-0.0000*ones(nx)>=0;t_d-0.0000*ones(T,1)>=0];
-constr= [ S>=0.000*eye(nd); LMI>=0; B_x>=0;t_d>=0];
+LMI=[M11 [-c';zeros(nz,nd);eye(nd)];
+     [-c';zeros(nz,nd);eye(nd)]' S];
+
+constr= [ S>=0.000*eye(nd); LMI>=0; B_z>=0;t_d>=0];
 % ops = sdpsettings('solver', 'scs','verbose',1);
-ops = sdpsettings('verbose',0);
+ops = sdpsettings('solver', 'sdpt3','verbose',0);
 % ops.scs.rho_x=0.09;
 % ops.scs.alpha=.05;
-ops.scs.rho_x=0.001;
-ops.scs.alpha=0.1;
-ops.scs.max_iters=20000;
+% ops.scs.rho_x=0.001;
+% ops.scs.alpha=0.1;
+% ops.scs.max_iters=20000;
 
 % ops.sedumi.beta=0.8;
 % ops.sedumi.theta=0.1;
@@ -60,13 +56,11 @@ ops.scs.max_iters=20000;
 % ops.sedumi.theta=0.5;
 
 % sol=optimize(constr, 10000*trace(S), ops);
-sol=optimize(constr, 1000*S(1,1), ops);
-S=abs(Pd*value(S)*Pd');
-% S=value(S)+0.0000000*eye(nd);
-c=Pd*value(c);
+sol=optimize(constr, 1000*trace(S), ops);
+S=abs(inv(Pd)*value(S)*inv(Pd)');
 
-% value(B_x)
-% value(t_d)
+c=Pd\value(c);
+
 
 
 end
--- RPI/ErrInv.m
+++ RPI/ErrInv.m
@@ -11,57 +11,69 @@ Wstate = warning('off');
 
 
 %% Transform the continuous-time state-space model into discretized state-space model
-Ts = 0.2; % Sample time
-% [Ad, Bd] = c2d(Ac,Bc,Ts);
+Ts = 0.1; 
+L=2.0;
+
 v_bar=15;
-L=2;
-Ad=[1 0 0;
+v_max=18;
+epsi_max=pi/8;
+ey_max=5;
+del_max=pi/8;
+curv=0;
+curv_bnd=0;
+
+
+
+Ad=[1 Ts*curv 0;
     0 1 Ts*v_bar;
-    0 0 1];
+    0 -Ts*v_bar*curv 1];
 
 Bd=Ts*[1 0;
        0 0;
-       0 v_bar/L];
+      -curv v_bar/L];
   
 %% Define LQR cost matrices
 
-Q = diag([100 100 500]);
-R = diag([10 10]);
+% Q = diag([100 10 200]);
+% R = diag([1 0.1]);
+
+% Q = diag([10 200 100]);
+% R = diag([.1 10]);
+
+% Q = diag([100 500 500]);
+% R = diag([100 200]);
+Q = diag([100 500 500]);
+R = diag([100 200]);
 
 %% Compute the state feedback gain F for LQR controller
 [K, P] = dlqr(Ad,Bd,Q,R);
 F = -K;
-%% Constraints on input
-u1max  = 1;
-u2max  = pi/10;
-u1min  = -1;
-u2min  = -pi/10;
-
-%% Constraints on states
-x1max = 1;
-x2max = 1;
-x3max = pi/8;
-x1min = 0;
-x2min = -1;
-x3min = -pi/8;
-
-%% State constraint set
-Hx = [1 0 0;-1 0 0;0 1 0;0 -1 0;0 0 1;0 0 -1];
-Kx = [x1max -x1min x2max -x2min x3max -x3min]';
-
-%% Input constraint set
-Hu = [1 0;-1 0;0 1;0 -1];
-Ku = [u1max -u1min u2max -u2min]';
+% [K1,P1]=dlqr(Ad(1,1), Bd(1,1), Q(1,1), R(1,1));
+% [K2, P2]=dlqr(Ad(2:end,2:end), Bd(2:end,2), Q(2:end,2:end), R(2,2));
+% F=[-K1 0 0;0 -K2];
 
 %% Disturbance set
+
 Hd = [1 0 0;-1 0 0;0 1 0;0 -1 0;0 0 1;0 0 -1];
-Kd = [Ts/20 Ts/20 Ts^2*v_bar/20*3 Ts^2*v_bar/20*3  Ts*v_bar/20 Ts*v_bar/20]';
+ds_max= max(-2.,0.5*(2*curv_bnd^2*ey_max^2+max(-2*curv_bnd*(v_max-v_bar),2*curv_bnd*(v_max-v_bar)))*Ts);
+ds_min= min(2.,0.5*(-epsi_max^2*v_bar+min(-2*curv_bnd*(v_max-v_bar),2*curv_bnd*(v_max-v_bar)))*Ts);
+
+dey_max=0.5*(v_max-v_bar)*epsi_max*Ts;
+dey_min=-0.5*(v_max-v_bar)*epsi_max*Ts;
+
+depsi_max= 0.5*(2*abs(curv_bnd)^3*ey_max^2*v_bar+curv_bnd*epsi_max^2*v_bar+2/L*(v_max-v_bar)*del_max...
+            +2*curv_bnd^2*(v_max-v_bar)*ey_max)*Ts;
+depsi_min= 0.5*(-2*abs(curv_bnd)^3*ey_max^2*v_bar-curv_bnd*epsi_max^2*v_bar-2/L*(v_max-v_bar)*del_max-2*curv_bnd^2*(v_max-v_bar)*ey_max)*Ts;
+
+Kd = [ds_max -ds_min dey_max -dey_min  depsi_max -depsi_min]';
+% Kd = [Ts/20 Ts/20 Ts^2*v_bar/20*3 Ts^2*v_bar/20*3  Ts*v_bar/20 Ts*v_bar/20]';
 Kd =Kd+d;
+% Kd= max(Kd, 0.001);
 %% Constraint set of S, W
 fA = Ad+Bd*F;
-Hs = [Hx;Hu*F];
-Ks = [Kx;Ku];
-S = Polyhedron('A',Hs,'B',Ks);
+% Hs = [Hx;Hu*F];
+% Ks = [Kx;Ku];
+% S = Polyhedron('A',Hs,'B',Ks);
 W = Polyhedron('A',1*Hd,'B',real(1*Kd));
 
 %% Compute mRPI
@@ -69,17 +81,26 @@ W = Polyhedron('A',1*Hd,'B',real(1*Kd));
 
 OmegaK =W; % Set the initial Omega
 maxiter = 100; % Set the maximum iterations  
+
+% OmegaKs =Ws;
+% OmegaKl=Wl;
 for i = 1:maxiter 
 
     Reach = fA*OmegaK;
     OmegaKplusOne = Reach+W;
-    if OmegaKplusOne == OmegaK
-        fprintf(['Completed at iteration ' num2str(i) ' \n'])
-        break 
-    end
+     if OmegaK==OmegaKplusOne 
+            fprintf(['Completed at iteration ' num2str(i) ' \n'])
+            break 
+     end
+%     
     % the OmegaKplusOne we've got in this iteration will be the OmegaK for 
     % the next iteration
     OmegaK = OmegaKplusOne;
+
+%     fprintf([ num2str(i) '\n'])
+
+
+
 end
 mRPI = OmegaKplusOne;
 
@@ -88,7 +109,7 @@ mRPI = OmegaKplusOne;
 % plot(mRPI, 'color', 'r', 'alpha', 0.2, W, 'color', 'b', 'alpha', 0.8, mRPI.outerApprox(), 'color', 'g', 'alpha', 0.01)
 % title('mRPI')
 % axis equal
-E=mRPI;
+E=mRPI.minHRep();
 %% Restore warning state
 warning(Wstate);
 
